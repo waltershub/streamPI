ad464d410acc4acb57ceb963ba5fa73c93c11145
Walter Shub <waltershub@gmail.com>
2020-08-25 22:34:41 -0400

    updated methods

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const Peer = require("peerjs-nodejs");
+var raspividStream = require("raspivid-stream");
+const peer = new Peer("Howl1234");
+var stream = raspividStream();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,4 +12,14 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+  const conn = peer.connect("HowlClient1234");
+
+  videoStream.on("data", (data) => {
+    ws.send(data, { binary: true }, (error) => {
+      conn.on("open", () => {
+        conn.send(data);
+      });
+      if (error) console.error(error);
+    });
+  });
 });
